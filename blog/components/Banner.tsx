a123eb29cb6f02c0d1bbb39baa379af42002ba86
Niko Cherkezishvili <95968630+Niccolochezzo@users.noreply.github.com>
2023-04-11 21:40:05 +0400

    ...

---
+++ blog/components/Banner.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold  px-10 py-5 mb-10">
+      <div>
+        <h1 className="text-7xl">Weekly Blog</h1>
+        <h2 className="mt-5 md:mt-0">
+          The{" "}
+          <span className="underline decoration-4 decoration-[#f7ab0a]">
+            Internet
+          </span>{" "}
+          has no eraser
+        </h2>
+      </div>
+      <p className="mt-5 md:mt-2 text-gray-400 max-2-sm">
+        The casual conversational tone of a blog is what makes it particularly
+        dangerous
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ blog/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
+      <div>
+        <Link className="flex items-center space-x-2" href="/">
+          Niko Blog
+        </Link>
+      </div>
+      <div>
+        <Link
+          className="px-5 py-3 text-sm md:text-base bg-gray-900 text-[#f7ab0a] flex items-center rounded-full text-center"
+          href="https://www.instagram.com/nikocherkezi/"
+          target="_blank"
+        >
+          Contact me
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
