0e83201fe6a55df747ffc6d143bb23e189bd8021
Niko Cherkezishvili <95968630+Niccolochezzo@users.noreply.github.com>
2023-04-12 16:40:27 +0400

    s

--- blog/components/BlogList.tsx
+++ blog/components/BlogList.tsx
@@ -14,7 +14,7 @@ function BlogList({ posts }: Props) {
 
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
-          <ClientSideRoute route={`/post/${post.slug.current}`}>
+          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
             <div className="group" key={post._id}>
               <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out cursor-pointer">
                 <Image
